a982ac6cd6d8234acb403d2ca8bee278c16e5c5e
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-11-25 15:06:05 -0400

    tests for bounceCoefficient

--- tests/ftest.js
+++ tests/ftest.js
@@ -17,14 +17,14 @@ export default function init() {
   let square = Polygon.rectangle(50, 50);
 
   // Create a bunch of actors in a grid with random velocities
-  const numActors = 20;
-  const spacing = 60;
-  const startX = -300, startY = -300;
+
+  const spacing = 200;
+  const startX = -400, startY = -400;
   let count = 0;
-  for (let i = 0; i < 33; i++) {
-    for (let j = 0; j < 55; j++) {
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
       let name = `A${count}`;
-      let poly = Polygon.makeRegular(5+ Math.floor(Math.random() * 10), 5 + Math.floor(Math.random() * 10));
+      let poly = Polygon.makeRegular(20, 5 + Math.floor(Math.random() * 50));
       let a = new Actor(name, poly);
       a.position = new Point(startX + i * spacing, startY + j * spacing);
       a.spin = Math.random() * 20 - 10;
@@ -34,14 +34,13 @@ export default function init() {
         `hsl(${Math.floor(Math.random() * 360)},80%,60%)`,
         "#fff"
       );
-      // Random velocity
-
       let angle = Math.random() * Math.PI * 2;
-      let speed = 1 + Math.random() * 15;
-      a.velocity = new Point(Math.cos(angle) * speed, Math.sin(angle) * speed);
-      a.bounceCoefficient = 0.001;// - Math.random()/10;
+      let speed = 0// + Math.random() * 15;
+      a.velocity = Point.zero();
+      a.bounceCoefficient = Math.ceil(1, Math.random()*2);
       a.setLabel(name, new Point(0, 0), 1);
       Director.addActor(a);
+      Director.addFieldToActor(a, -a.mass() * 10);
       count++;
     }
   }
