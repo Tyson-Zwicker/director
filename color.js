fd2504a40ea7c11a4c9371b2c3345af3cf44bf28
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-11-27 20:00:28 -0400

    added brightness modifier

--- color.js
+++ color.js
@@ -1,25 +1,42 @@
 export default class Color {
     //We are going to store the values, internally, as decimal between 1 and 0.
     //Well give them back either as #RBGA Strings.
-
+    #isColor = false;
     constructor(r, g, b, opacity = 1) {
         if (isNaN(r) || isNaN(g) || isNaN(b)) throw new Error(`color.constructor: Color component undefined R${r} G${g} B${b}`);
         if (r < 0 || r > 15 || g < 0 || g > 15 || b < 0 || b > 15) throw new Error(`color.constructor: rgb must be 0>=value<=15 R${r} G${g} B${b}`)
-        if (opacity && opacity < 0 || opacity > 1) throw new Error(`Color.constructor: opacity must be between 0 and 1 [${opacity}`);
+        if (opacity === null || opacity === undefined) opacity = 1;
+        if (opacity < 0 || opacity > 1) throw new Error(`Color.constructor: opacity must be between 0 and 1 [${opacity}]`);
         this.r = r;
         this.g = g;
         this.b = b;
         this.opacity = opacity;
-        if (!opacity) this.opacity = 1;
-        if (opacity < 0 || opacity > 1) throw new Error('transparency must be a number between 0 and 1');
+        this.#isColor = true;
     }
     toString() {
         let r = `r:${this.r} g:${this.g} b:${this.b} a:${this.opacity} hex:${this.asHex()}`;
         return r;
     }
-    getLinearGradient(x1, y1, x2, y2, stopPoint1, color1, stopPoint2, color2, stopPoint3, color3, stopPoint4, color4) {
+    isColor() {
+        return this.#isColor;
+    }
+    //negative number makes it brighter or dimmer. Range is -1 to 1, by a percetage of the existing value.
+    changeBrightness (c){
+        if (isNaN(c) || c<0 || c>1) throw new Error(`color.changeBrightness: change should be a value between 0 and 1 ${c}`);        
+        let dr = this.r * c;
+        let dg = this.g * c;
+        let db = this.b * c;
+        let nr = this.r + dr;
+        let ng = this.g + dg;
+        let nb = this.b + db;
+        nr = Math.max (0,nr); nr = Math.min (15,nr);
+        ng = Math.max (0,ng); ng = Math.min (15,ng);
+        nb = Math.max (0,nb); nb = Math.min (15,nb);
+        return new Color (nr,ng,nb,this.a)
+    }
+    getLinearGradient(context,x1, y1, x2, y2, stopPoint1, color1, stopPoint2, color2, stopPoint3, color3, stopPoint4, color4) {
         if (isNaN(x1) || isNaN(x2)) throw new Error(`line.draw bad coordinates (${x1},${y1}) (${x2},${y2})`)
-        let gradient = this.g.createLinearGradient(x1, y1, x2, y2);
+        let gradient = context.createLinearGradient(x1, y1, x2, y2);
         if (stopPoint1 && color1) {
             if (isNaN(stopPoint1)) throw Error(`draw.setLinearGradient: ${stopPoint1} is not a number between 0 and 1`)
             gradient.addColorStop(stopPoint1, color1.color);
