b6555046f865ac4e3f3073635e33ce5ac4dd3177
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-11-29 22:09:49 -0400

    syntax

--- boundry.js
+++ boundry.js
@@ -14,15 +14,14 @@ export default class Boundry {
             actor.position.y + actor.radius()
         );
     }
-
     touches(otherBoundry) {
-       return (
+        return (
             ((otherBoundry.x2 > this.x1 && otherBoundry.x2 < this.x2) ||
                 (otherBoundry.x1 > this.x1 && otherBoundry.x1 < this.x2)) ||
             ((otherBoundry.y2 > this.y1 && otherBoundry.y2 < this.y2) ||
                 (otherBoundry.y1 > this.y1 && otherBoundry.y1 < this.y2)));
     }
-    isBoundry(){
-        return (this.x2>this.x1) && (this.y2>this.y1) && (!isNaN (this.x1+this.x2+this.y1+this.y2)); 
+    isBoundry() {
+        return (this.x2 > this.x1) && (this.y2 > this.y1) && (!isNaN(this.x1 + this.x2 + this.y1 + this.y2));
     }
 }
