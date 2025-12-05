c5c929049518c02c104b309e37676fd1011a24bd
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-05 10:48:56 -0400

    completey untested!

--- sensor.js
+++ sensor.js
@@ -1,39 +1,68 @@
 import Director from './Director.js';
 import Point from './points.js';
-import LineEffect from './lineeffects.js';
-
+import Director from './director.js';
+import QuadTree from '.quadtree.js';
+import Boundry from 'boundry.js';
 export default class Sensor {
   //Direction is a component vector
   //returns "false" or an object with "position" and "distance".
-  constructor (actor, angle1, angle2, speed, strength){
+  //angle is its centerline. sweep is degrees to either side of centerline
+  //speed is degrees per second
+  //strength is used to calc the distance 
+  constructor(actor, angle, sweep, speed, distance) {
     this.actor = actor;
-    this.angle1 = angle1;
-    this.angle2 = angle2;
-    this.angleCurrent = undefined;
+    this.angle = angle;
+    this.sweep = sweep;
+    this.currentAngle = angle;
+    this.currentDirection = 1;
     this.speed = speed;
-    this.strength = strength;
+    this.distance = distance;
   }
-  //Use the quad tree to get a list of potential
-  //sensor candidates.
-  /// MOVE THIS TO Actor so it doesn't recalc this list
-  //more than it needs to.  When an actor gets a new
-  //sensor it should update a maxPossible range (or whatever)
 
-  getCandidates (){
-
-  }
-  sweep (delta){
-    if (angle ===undefined){
-      //figure out an angle set in the middle of and keep
-      //in mind angles are modal, integers, etc.. keep it
-      //within 360..
-    }
-    //move angle by delta, between angle1 and angle2
+  sweep(delta) {
     //Get the candidates and go through the list.
-    //return the one with the shortest distance.
+    let cx = this.actor.position.x;
+    let cy = this.actor.position.y;
+    let sensorBoundry = new Boundry(cx - this.distance, cy - this.distance, cx + this.distance, cy + this.distance);
+    let foundActors = Director.quadtree.findInBounds(sensorBoundry);
 
+    let results = this.#examineCandidates();
+
+    //move sensors currentAngle, send it back the otherway when it reaches its the edge of its sweep
+    this.currentAngle += delta;
+    if (this.currentAngle > this.sweep + this.direction) {
+      this.currentAngle = this.sweep + this.direction * delta;
+      this.direction *= -1;
+    }
+    return closestActor;
   }
-  static canSee(originPoint, direction, barrierPoint1, barrierPoint2, addLineEffect) {
+  #examineCandidates(foundActors) {
+    let closestDistance = Number.MAX_SAFE_INTEGER;
+    let closestActor = undefined;
+    for (actor in foundActors) {
+      let points = actor.polygon.points;
+      for (let i = 0; i < points.length; i++) {
+        let barrierPoint1 = undefined;
+        let barrierPoint2 = undefined;
+        if (i == points.length - 1) {
+          //connect last point to first point.
+          barrierPoint1 = p[i];
+          barrierPoint2 = p[i + 1];
+        } else {
+          //connect all but last point to next point
+          barrierPoint1 = p[i];
+          barrierPoint2 = p[0];
+        }
+        let result = this.#rayCast(this.actor.position, this.direction + this.actor.rotation, barrierPoint1, barrierPoint2);
+        if (result && result.distance < closestDistance) {
+           result[actor] = actor; //Add this information to the sensor results..
+        }
+      }
+      return result;
+    }
+    //return the one with the shortest distance.
+  }
+  #rayCast(originPoint, direction, barrierPoint1, barrierPoint2) {
     const x1 = barrierPoint1.x;
     const y1 = barrierPoint1.y;
     const x2 = barrierPoint2.x;
@@ -52,20 +81,11 @@ export default class Sensor {
     if (t > 0 && t < 1 && u > 0) {
       const p = new Point(x1 + t * (x2 - x1), y1 + t * (y2 - y1));
       const d = Point.distance(originPoint, p);
-      if (addLineEffect) {
-        Director.addForegroundEffect(
-          originPoint,
-          p,
-          1,
-          new Color(15, 15, 15),
-          0.5
-        );
-      }
       return {
         position: p,
         distance: d
       };
-    };
+    }
     return false; //They don't ever hit.
   }
 }
