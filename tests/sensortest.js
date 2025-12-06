6c34d27d81c9e002e2e22e8d9d835333d5e638d7
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-06 14:22:43 -0400

    tests environment for sensors.

---
+++ tests/sensortest.js
@@ -0,0 +1,73 @@
+
+import Point from '../point.js';
+import Polygon from '../polygon.js';
+import Button from '../button.js';
+import Actor from '../actor.js';
+import Director from '../director.js';
+import Part from '../part.js';
+import Appearance from '../appearance.js';
+
+// PRIME MOVER
+document.addEventListener('DOMContentLoaded', () => {
+  Director.initialize();
+  init();
+});
+
+export default function init() {
+  let sensorsActive = false;
+
+  let a = new Actor(`Scout Craft`, Polygon.triangle(50, 100));
+  a.position = new Point(-200, -200);
+  a.spin = 1;
+  a.rotation = 0;
+  a.appearance = new Appearance('#066', '#06f', '#fff');
+  let part1 = new Part('box', -25, -20, Polygon.rectangle(15, 5), 0);
+  part1.appearance = new Appearance('#ff0', '#f00', '#fff');
+  a.attachPart(part1);
+  let part2 = new Part('box', -25, 20, Polygon.rectangle(15, 5), 0);
+  part2.appearance = new Appearance('#0f7', '#0ff', '#fff');
+  a.attachPart(part2);
+  Director.addActor(a);
+  let hovered = new Appearance ('#ff9','#f00','#fff',2);
+  let pressed = new Appearance ('#6ff','#f82','#fff',3);
+  let sensorToggleButton = new Button (hovered,pressed);
+  sensorToggleButton.clickFn = function toggleSensors(){
+    sensorsActive = !sensorsActive;
+  }
+  a.attachButton (sensorToggleButton);
+
+  let b = new Actor(`BigRed`, Polygon.makeIrregular(70, 500,700));
+  b.position = new Point(3000, 0);
+  b.spin = 2;
+  b.rotation = 30;
+  b.appearance = new Appearance('#f06', '#f00', '#f72');
+  Director.addActor(b);
+
+  let c = new Actor(`BigBlue`, Polygon.makeRegular(30, 1500));
+  c.position = new Point(-3000, -3000);
+  c.spin = 3;
+  c.rotation = 60;
+  c.appearance = new Appearance('#03F', '#09f', '#0af');
+  Director.addActor(c);
+
+  let d = new Actor(`BigPurple`, Polygon.makeRegular(30, 1000));
+  d.position = new Point(200, 6000);
+  d.spin = 4;
+  d.rotation = 90;
+  d.appearance = new Appearance('#b0d', '#f0f', '#f0f');
+  Director.addActor(d);
+  
+  function runTheTest() { 
+    if (sensorsActive){
+      part1.rotation += 5;
+      part2.rotation -= 5;
+    }
+  }
+  Director.addCreatorsFunction(runTheTest);
+  Director.run();
+}
+
+
+function rnd(min, max) {
+  return Math.floor(min + Math.random() * (Math.abs(max) - min));
+}
