23f1fe55857fdf85ebff0d584a57680c1aeb2440
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-06 22:21:29 -0400

    fixed the orbits.

--- tests/orbits.js
+++ tests/orbits.js
@@ -17,14 +17,14 @@ export default function init() {
 
   //Try to make them orbit.
 
-  let star = new Actor('Star', Polygon.makeRegular(40, 1000), new Appearance('#FFA', '#FFF', "Star", 3));
+  let star = new Actor('Star', Polygon.makeRegular(40, 1100), new Appearance('#FFA', '#FFF', "Star", 3));
   star.bounceCoefficient =0.1;
   Director.addActor(star);
   Director.addFieldToActor(star, star.mass() * - 250);
 
   let planet = new Actor('Planet', Polygon.makeRegular(12, 10), new Appearance('#F90', '#FF0', 3));
   planet.position = new Point(1800, 0);
-  planet.velocity = new Point(0,260);
+  planet.velocity = new Point(0,290);
   planet.bounceCoefficient = 0.1;
   Director.addActor(planet);
   Director.addFieldToActor(planet, planet.mass()*-1);
@@ -36,9 +36,9 @@ export default function init() {
   Director.addActor(planet3);
   Director.addFieldToActor(planet3, planet3.mass()*-1);
 
-  let planet2 = new Actor('Planet3', Polygon.makeRegular(20, 50), new Appearance('rgba(93, 44, 61, 1)', '#FFF', 3));
+  let planet2 = new Actor('Planet3', Polygon.makeRegular(20, 150), new Appearance('rgba(93, 44, 61, 1)', '#FFF', 3));
   planet2.position = new Point(2800, 0);
-  planet2.velocity = new Point(0,50);
+  planet2.velocity = new Point(0,24);
   planet2.bounceCoefficient = 0.1;
   Director.addActor(planet2);
   Director.addFieldToActor(planet2, planet2.mass()*-1);
