7b380ad14f1b23a311732b10631d99cde9416303
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-08 03:07:19 -0400

    encapsulated everything (i hope) into a class

---
+++ sensorresponse.js
@@ -0,0 +1,28 @@
+export default class SensorResponse {
+  time = undefined;               //Timestamp of signal
+  sensor = undefined;             //Sensor that MADE the signal
+  actor = undefined;              //Actor that owns the sensor
+  distance = Number.MAX_SAFE_INTEGER;//Distance between actor and closest part of "detectedActor"'s polygon
+  locationOfOrigin = undefined;   //Where owner was when signal was generated.
+  locationOfResponse = undefined; //Where the sensor pinged back from.
+  detectedActor = undefined;      //Who the sensor pinged back from.
+  worldAngle = undefined;         //Angle (in world space) the sensor was looking.
+  actorOrientation = undefined;   //heading of sensor owner
+  offsetAngle = undefined;        //sensor offset at the time.
+
+  constructor(time, sensor, actor, locationOfOrigin, worldAngle, actorOrientation, offsetAngle) {
+    this.time = time;
+    this.sensor = sensor;
+    this.actor = actor;
+    this.distance = distance;
+    this.locationOfOrigin = locationOfOrigin;
+    this.worldAngle = worldAngle;
+    this.actorOrientation = actorOrientation;
+    this.offsetAngle = offsetAngle;
+  }
+  setResponse (locationOfResponse, distance, detectedActor){
+    this.locationOfResponse = locationOfResponse;
+    this.distance = distance;
+    this.detectedActor = detectedActor;
+  }
+}
