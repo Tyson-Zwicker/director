d09d74a4e140cf43ce160e834554754a65e0536e
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-09 14:12:04 -0400

    added 'held' to 'press' and 'release'.  'Held' comes with an extra 'duration' property.

--- keyboard.js
+++ keyboard.js
@@ -3,15 +3,28 @@ import EventTracker from './eventtrackers.js';
 export default class Keyboard {
   static initialize() {
     Keyboard.functions = new Map();
+    Keyboard.events = new Map();
     window.onkeydown = function (e) {
-      Keyboard.events.add({ key: e.key, when: Date.now(), action: 'press' });
+      let eventInfo = {};
+      if (Keyboard.events.has(e.key)) {
+        eventInfo[key] = e.key;
+        eventInfo[when] = Datetime.now;
+        let lastEventOnThisKey = Map.get(e.key);
+        if (lastEventOnThisKey.action === 'press' || lastEventOnThisKey.action === 'held') {
+          eventInfo[action] = 'held';
+          eventInfo[duration] = eventInfo[when] - lastEventOnThisKey.when;
+        }
+      } else {
+        eventInfo[action] === 'press'
+      }
+      Keyboard.events.set(eventInfo);
       if (Keyboard.functions.has(e.key)) {
         let keyFunction = Keyboard.functions.get(e.key);
-        keyFunction();
+        keyFunction(eventInfo);
       }
     }
     window.onkeyup = function (e) {
-      Keyboard.events.add({ key: e.key, when: Date.now(), action: 'release' });
+      Keyboard.events.set({ key: e.key, when: Date.now(), action: 'release' });
       if (Keyboard.functions.has(e.key)) {
         let keyFunction = Keyboard.functions.get(e.key);
         keyFunction();
