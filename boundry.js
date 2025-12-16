e7588a83d5df6d80589f7ee69ed3d62164680711
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-16 18:54:07 -0400

    making extra sure it works.

--- boundry.js
+++ boundry.js
@@ -17,12 +17,32 @@ export default class Boundry {
       actor.position.y + actor.radius()
     );
   }
+  isInside(x, y) {
+    if (x > this.x1 &&
+      x < this.x2 &&
+      y > this.y1 &&
+      y < this.y2) return true;
+    return false;
+  }
   touches(otherBoundry) {
+    //Corner cases (will also catch if other boundry is INSIDE this)
+    //case 1: otherboundry.x1,y1 inside this.
+    //case 2: otherboundry.x1,y2 inside this.
+    //case 3: otherboundry.x2,y1 inside this.
+    //case 4: otherboundry.x1,y2 inside this.
+    //return true if ANY of the above are true..
+
+    if (this.isInside (otherBoundry.x1, otherBoundry.y1)) return true;
+    if (this.isInside (otherBoundry.x1, otherBoundry.y2)) return true;
+    if (this.isInside (otherBoundry.x2, otherBoundry.y1)) return true;
+    if (this.isInside (otherBoundry.x2, otherBoundry.y2)) return true;
+
     return (
       ((otherBoundry.x2 > this.x1 && otherBoundry.x2 < this.x2) ||
         (otherBoundry.x1 > this.x1 && otherBoundry.x1 < this.x2)) ||
       ((otherBoundry.y2 > this.y1 && otherBoundry.y2 < this.y2) ||
         (otherBoundry.y1 > this.y1 && otherBoundry.y1 < this.y2)));
+    //The only case it doesn't detect is if THIS is INSIDE other other boundry,
+    //but I don't that I want it to...
   }
-
 }
