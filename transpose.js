e1f9565425cfb6945328e923d03dace06d9f5845
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-15 23:31:09 -0400

    centralizing all linear transformations here. one more method to write, and a lot of very gentle integration and testing...

---
+++ transpose.js
@@ -0,0 +1,40 @@
+import Point from './point.js';
+export default class Transpose {
+  //For finding the origin of a POINT, given its own position(offset) and its parents position AND FACING
+  //Returns WORLD coordinates.
+  static pointToWorld(point, parent) {
+    let p = Point.from(parent.position);
+    let o = Point.from(point);
+    Point.rotate(o, parent.facing);
+    Point.add(p, o);
+    return p;
+  }
+  //NO PARENT: //Just moves a damn point to the Director.view.camera location..
+  //IF parent defined:
+  //For finding the origin of a POINT, given its own position(offset) and its parents position AND FACING
+  //-->AND THEN move it to screen coords.
+  static pointToScreen(point, parent) {
+    let p = Point.from(parent);
+    let o = Point.from(this.position);
+    Point.scale(o, Director.view.camera.zoom);
+    Point.add(p, o);
+    Point.sub(p, Director.view.camera);
+    Point.scale(p, Director.view.camera.zoom);
+    Point.add(p, director.view.screenCenter);
+
+  }
+  //UNLIKE POINTS.. children have a facing.  THis moves the point, but it considers the child's facing as well.
+  //The "child" is the object that owns the "point" being drawn.
+  static childToWorld(point, child, parent) {
+    let childOrigin = Transpose.pointToWorld(child.position, parent);
+    let p = Point.from(point);
+    let angle = child.facing + parent.facing;
+    Point.rotate(p, angle);
+    Point.add(p, childOrigin);
+    return p;
+  }
+  static childToScreen(point, child, parent) {
+    //TODO:  (See: Polygon.draw) 2025-12-15:23:30
+    
+  }
+}
