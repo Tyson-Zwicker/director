f57cd6c91b288747bbf7eb2a71f8efcf957feb9e
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-26 21:02:48 -0400

    added circle and 'box2'

--- draw.js
+++ draw.js
@@ -3,6 +3,20 @@ export default class Draw {
   constructor(context2d) {
     this.g = context2d;
   }
+  box2(x1, y1, x2, y2, hexColor) {
+    if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) throw new Error(`line.draw bad: coordinates (${x1},${y1}) (${x2},${y2})`);
+    
+    this.g.strokeStyle = hexColor;
+    this.g.beginPath();
+    this.g.rect(x1, y1, x2 - x1, y2 - y1);
+    this.g.stroke();
+  }
+  circle (x,y,radius, hexColor){
+    this.g.fillStyle = hexColor;
+    this.g.beginPath();
+    this.g.arc (x,y,radius,0,Math.PI*2);
+    this.g.fill();    
+  }
   box(x1, y1, x2, y2, colorOrGradient) {
     if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) throw new Error(`line.draw bad: coordinates (${x1},${y1}) (${x2},${y2})`);
     if (colorOrGradient instanceof Color) {
