2c5099d411a859ea4176c672948706ecac9d1dca
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-26 23:28:34 -0400

    almost ready for testing..

--- sensor.js
+++ sensor.js
@@ -1,67 +1,66 @@
-import Point from './point.js';
-import Director from './director.js';
-import Boundry from './boundry.js';
-import Actor from './actor.js';
-import LineEffect from './lineeffect.js';
-import Color from './color.js';
-import SensorResponse from './sensorresponse.js';
-import Transpose from './transpose.js';
 class Sensor {
-  constructor(range, facing, fieldOfView, parent) {
+  constructor(owner, range, sweepArc, facing, sweepSpeed) {
+    this.sweepArc = sweepArc;
+    this.facing = facing;
+    this.halfArc = sweepArc/2; //to avoid dividing by 2 a lot..
     this.range = range;
-    this.facing = facing; //degrees
-    this.fieldOfView = fieldOfView;
-    this.halfView = fieldOfView / 2;  //degrees to either side of the facing.. to avoid multiplying by 2 every time.
-    this.owner = parent;
+    this.sweepDirectionofRotation = 1; //either 1 or -1 (sweeping between the two angles back and forth..)
+    this.sweepAngle = sweepArc / 2; //This is the direction the beam is look now. (always between -halfSweepArc and +halfSweepArc)
+    this.sweepSpeed = sweepSpeed; //How many degrees per sweep motoin the beam angle changes..
+    this.owner = owner;
   }
-  detect() {
-    //Find the end-points of the line segments defining this sensor field of view.
-    let sensorEdge1 = new Point(1, 0);
-    Point.rotate(sensorEdge1, this.owner.owner.facing + this.owner.facing + this.fieldOfView / 2);
-    Point.scale(sensorEdge1, this.range);
-    let sensorEdge2 = new Point(1, 0);
-    Point.rotate(sensorEdge2, this.owner.owner.facing + this.owner.facing - this.fieldOfView / 2);
-    Point.scale(sensorEdge2, this.range);
-    let sensorCenterEdge = new Point(1, 0);
-    Point.rotate(sensorCenterEdge, this.owner.owner.facing + this.owner.facing);
-    Point.scale(sensorCenterEdge, this.range);
-
-    //Now get the objects that could potentially be in sight based on range.
-    let sensorBoundry = new Boundry(
-      this.owner.position.x - this.range,
-      this.owner.position.y - this.range,
-      this.owner.position.x + this.range,
-      this.owner.position.y + this.range);
-    let candidates = Director.quadtree.findInRange(sensorBoundry);
-
-    //Sort them by distance in ascending order using "Chebyshev Distance" 
-    //It is somewhat inaccurate, but very fast...
-    candidates.sort((a, b) => {
-      // Chebyshev Distance = max(|x1 - x2|, |y1 - y2|)
-      const distA = Math.max(
-        Math.abs(a.position.x - this.owner.position.x),
-        Math.abs(a.position.y - this.owner.position.y)
-      );
-      const distB = Math.max(
-        Math.abs(b.position.x - this.owner.position.x),
-        Math.abs(b.position.y - this.owner.position.y)
-      );
-      return distA - distB;
-    });
-
-    //Are they bounded within the two lines segments?  If so, put them in the "detected" list,
-    //and refine the sensorEdges to the angles the won't intersect the detected object.
-
-    //TODO: I need a function that can take two points and turn them into a "line" that can
-    //then be asked "where is x if y=?? and also where is y if x=??" in order to calculate the
-    //boundry. DIGEST THIS:  https://en.wikipedia.org/wiki/Linear_equation 
-
-    //Consider scenarios.  Object is DIRECTLY above (x2-x1 =0) or beside (y2-y1=0).
-    //And then squarely in each quadrant.  Are there edge cases?
-
-
-    for (let candidate of candidates) {
-
+  detect(){
+    let trueFacing = this.owner.facing + this.facing + this.sweepAngle;
+    let rayEndPoint = new Point.fromPolar (trueFacing, this.range);
+    let candidates = this.#getSortedObjectsWithinRange(rayEndPoint);
+    for (let candidate of candidates){
+      if (this.#canSee(candidate)) {
+        this.#drawPingReturned(candidate);
+        return candidate; //don't look past the first one you see, because anything else will be blocked.        
+      }
+      this.#drawPingToEdge (rayEndPoint);
     }
+    this.#sweep;
+  }
+  #drawPingReturned (trueFacing, candidate){
+      let rayPoint = Point.fromPolar (trueFacing, candidate.distance)
+      let ray = new LineEffect(this.actor.position, closestPoint, 2, new Color(0, 15, 0, 1), 2);
+      Director.addForegroundEffect(ray);  
+  }
+  #drawPingToEdge (rayEndPoint){
+    let ray = new LineEffect(this.actor.position, rayEndPoint, 2, new Color(7, 7, 7, 1), 2);
+    Director.addForegroundEffect(ray);
+  }
+  #getSortedObjectsWithinRange(rayEndPoint){    
+    let sensorBoundry = new Boundry (owner.position.x,owner.position.y,rayEndPoint.x, rayEndPoint.y);//TODO: FIGURE THIS OUT BASED ON THE FACING AND RANGE
+    //Use Quadtree to get the candidates for detection    
+    let candidates = Director.quadtree.findInRange(sensorBoundry);
+    //add a distance property based on their distance to the origin..
+    for (let candidate of  candidates){
+      candidate["distance"] = Point.distance (owner.position, rayEndPoint);
+    }
+    //return that array, sorted from closest to farthest
+    return candidates.sort ((a,b)=>{return a.distance - b.distance});
+  }
+  #sweep() {
+    this.sweepAngle += this.sweepSpeed * this.sweepDirectionofRotation;
+    if (sweepAngle >= sweepArc) {
+      sweepAngle = sweepArc;
+      sweepDirectionOfRotation = -1;
+    }
+    if (sweepAngle < 0) {
+      sweepAngle = 0;
+      sweepAngle = 1;
+    }
+  }
+  
+ 
+  #canSee(actor) {
+    //TODO:
+    //get the angle to the center of obj from origin.
+    //If that angle is within a degree of the sweepAngle - return true.
+    //get a the line segments tangential to the line from origin to obj.
+    //the first line segment goes to the "right" of the obj position, the other to the left.
+    //If line coming from the origin intersects either line segment, return true.
   }
 }
