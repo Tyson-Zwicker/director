2151ad26835c8a7a23dcd0bfe9872517c5047730
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-26 21:28:02 -0400

    removed old code

--- boundry.js
+++ boundry.js
@@ -18,14 +18,6 @@ export default class Boundry {
   isBoundry() {
     return (this.x2 > this.x1) && (this.y2 > this.y1) && (!isNaN(this.x1 + this.x2 + this.y1 + this.y2));
   }
-  static getActorBounds(actor) {
-    return new Boundry(
-      actor.position.x - actor.radius(),
-      actor.position.y - actor.radius(),
-      actor.position.x + actor.radius(),
-      actor.position.y + actor.radius()
-    );
-  }
   isInside(x, y) {
     if (x > this.x1 &&
       x < this.x2 &&
@@ -38,10 +30,10 @@ export default class Boundry {
     if (otherBoundry.x1 > this.x1 && otherBoundry.x1 < this.x2 && otherBoundry.y1 > this.y1 && otherBoundry.y1 < this.y2) return true;
     //Is the other (x1,y2) in boundry?
     if (otherBoundry.x1 > this.x1 && otherBoundry.x1 < this.x2 && otherBoundry.y2 > this.y1 && otherBoundry.y2 < this.y2) return true;
-      //Is the other (x2,y1) in boundry?
+    //Is the other (x2,y1) in boundry?
     if (otherBoundry.x2 > this.x1 && otherBoundry.x2 < this.x2 && otherBoundry.y1 > this.y1 && otherBoundry.y1 < this.y2) return true;
-      //Is the other (x2,y2) in boundry?
+    //Is the other (x2,y2) in boundry?
     if (otherBoundry.x2 > this.x1 && otherBoundry.x2 < this.x2 && otherBoundry.y2 > this.y1 && otherBoundry.y2 < this.y2) return true;
-      return false;
+    return false;
   }
 }
