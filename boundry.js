555297bc84139bd0aabccb518e5e2c0a717a8766
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-26 21:02:04 -0400

    fixed false positive return

--- boundry.js
+++ boundry.js
@@ -6,6 +6,15 @@ export default class Boundry {
     this.x2 = x2;
     this.y2 = y2;
   }
+
+  get width() {
+    return this.x2 - this.x1;
+  }
+
+  get height() {
+    return this.y2 - this.y1;
+  }
+
   isBoundry() {
     return (this.x2 > this.x1) && (this.y2 > this.y1) && (!isNaN(this.x1 + this.x2 + this.y1 + this.y2));
   }
@@ -25,24 +34,14 @@ export default class Boundry {
     return false;
   }
   touches(otherBoundry) {
-    //Corner cases (will also catch if other boundry is INSIDE this)
-    //case 1: otherboundry.x1,y1 inside this.
-    //case 2: otherboundry.x1,y2 inside this.
-    //case 3: otherboundry.x2,y1 inside this.
-    //case 4: otherboundry.x1,y2 inside this.
-    //return true if ANY of the above are true..
-
-    if (this.isInside (otherBoundry.x1, otherBoundry.y1)) return true;
-    if (this.isInside (otherBoundry.x1, otherBoundry.y2)) return true;
-    if (this.isInside (otherBoundry.x2, otherBoundry.y1)) return true;
-    if (this.isInside (otherBoundry.x2, otherBoundry.y2)) return true;
-
-    return (
-      ((otherBoundry.x2 > this.x1 && otherBoundry.x2 < this.x2) ||
-        (otherBoundry.x1 > this.x1 && otherBoundry.x1 < this.x2)) ||
-      ((otherBoundry.y2 > this.y1 && otherBoundry.y2 < this.y2) ||
-        (otherBoundry.y1 > this.y1 && otherBoundry.y1 < this.y2)));
-    //The only case it doesn't detect is if THIS is INSIDE other other boundry,
-    //but I don't that I want it to...
+    //Is the other (x1,y1) in boundry?
+    if (otherBoundry.x1 > this.x1 && otherBoundry.x1 < this.x2 && otherBoundry.y1 > this.y1 && otherBoundry.y1 < this.y2) return true;
+    //Is the other (x1,y2) in boundry?
+    if (otherBoundry.x1 > this.x1 && otherBoundry.x1 < this.x2 && otherBoundry.y2 > this.y1 && otherBoundry.y2 < this.y2) return true;
+      //Is the other (x2,y1) in boundry?
+    if (otherBoundry.x2 > this.x1 && otherBoundry.x2 < this.x2 && otherBoundry.y1 > this.y1 && otherBoundry.y1 < this.y2) return true;
+      //Is the other (x2,y2) in boundry?
+    if (otherBoundry.x2 > this.x1 && otherBoundry.x2 < this.x2 && otherBoundry.y2 > this.y1 && otherBoundry.y2 < this.y2) return true;
+      return false;
   }
 }
