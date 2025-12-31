28aace3c53c9fc25831ab898fabbc04fbd1907f9
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-12-31 13:14:38 -0400

    handled single and no parameter instances by creating a range dynamically (floats defaults to range 0-1), one param to int or float creates range 0->min).

--- classes/rnd.js
+++ classes/rnd.js
@@ -1,30 +1,45 @@
 export default class Rnd {
+  //IF ONLY ONE provided, range is 0->min,
+  //otherwise its min->max
   static int(min, max) { // max not inclusive..
+    if (!max && typeof min === 'number') {
+      max = min;
+      min = 0;
+    }
     if (typeof min !== 'number' || typeof max !== 'number') {
       throw new TypeError('Rnd.int requires numeric parameters');
     }
     return Math.floor(min + Math.random() * (Math.abs(max) - min));
   }
 
+  //no params 0->1
+  //1 param 0->min
+  //2 param min->max
   static float(min, max) { //max not inclusive..
-    if (!max) max = 1;
-    if (!min) min = 0;
+    if (!max &&!min){
+      min=0;
+      max = 1;
+    }
+    if (!max){
+      max = min;
+      min = 0;
+    } 
     if (typeof min !== 'number' || typeof max !== 'number') {
       throw new TypeError('Rnd.float requires numeric parameters');
     }
     return min + Math.random() * (Math.abs(max) - min);
   }
   static hex(min, max, digits) { //0->15 but 0->F
-    if (!min || min<0) min = 0;
-    if (!max || max<0 || max>16) max = 16;
+    if (!min || min < 0) min = 0;
+    if (!max || max < 0 || max > 16) max = 16;
     if (typeof min !== 'number' || typeof max !== 'number' || (digits !== undefined && typeof digits !== 'number')) {
       throw new TypeError('Rnd.hex requires numeric parameters');
     }
-    if (digits && digits>1){
+    if (digits && digits > 1) {
       let d = 0;
-      let s= '';
-      while (d<digits){
-        s+=Rnd.hex (min,max);
+      let s = '';
+      while (d < digits) {
+        s += Rnd.hex(min, max);
         d++;
       }
       return s;
