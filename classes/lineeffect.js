fe84d33c200641a222f15eaea2b705ed5e382a3d
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-01-01 00:50:27 -0400

    change points from p1,p2 to p0,p1 for consistancy.

--- classes/lineeffect.js
+++ classes/lineeffect.js
@@ -3,20 +3,20 @@ import Director from './director.js';
 import Transpose from './transpose.js';
 import Point from './point.js';
 export default class LineEffect {
-  constructor(p1, p2, w, colorOrGradient, durationInSeconds) {
-    if (!Point.isPointy(p1) || !Point.isPointy(p2)) throw new Error(`LineEffect.constructor: Bad Point: p1(${p1}), p2(${p2})`)
+  constructor(p0, p1, w, colorOrGradient, durationInSeconds) {
+    if (!Point.isPointy(p0) || !Point.isPointy(p1)) throw new Error(`LineEffect.constructor: Bad Point: p0(${p0}), p1(${p1})`)
     if (typeof w !== 'number' || w<0) throw new Error(`Width must be a number >0 [${w}]`);
     if (typeof durationInSeconds !== 'number' || durationInSeconds<0) throw new Error(`Duration must be a number >0 [${durationInSeconds}]`);
+    this.p0 = p0;
     this.p1 = p1;
-    this.p2 = p2;
     this.w = w;
     this.colorOrGradient = colorOrGradient;
     this.duration = durationInSeconds;
     this.life = durationInSeconds;
   }
   draw(context, delta) {
+    let screenPoint0 = Transpose.worldToScreen(this.p0);
     let screenPoint1 = Transpose.worldToScreen(this.p1);
-    let screenPoint2 = Transpose.worldToScreen(this.p2);
     if (!context || isNaN(delta)) throw (`LineEffect.draw: bad params context ${context}, delta ${delta}`);
     if (this.colorOrGradient instanceof Color) {
       let color = this.colorOrGradient.withOpacity(this.life / this.duration);
@@ -26,8 +26,8 @@ export default class LineEffect {
     }
     context.lineWidth = this.w * Director.view.camera.zoom;
     context.beginPath();
-    context.moveTo(screenPoint1.x, screenPoint1.y);
-    context.lineTo(screenPoint2.x, screenPoint2.y);
+    context.moveTo(screenPoint0.x, screenPoint0.y);
+    context.lineTo(screenPoint1.x, screenPoint1.y);
     context.stroke();
     this.life -= delta;
     return (this.life > 0);
