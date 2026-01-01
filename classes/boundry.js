c163ea9b86cb13401fd0b2a351e93754b60d5ee8
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-01-01 00:49:18 -0400

    touches() is static, and works. reliably.

--- classes/boundry.js
+++ classes/boundry.js
@@ -40,23 +40,37 @@ export default class Boundry {
   isBoundry() {
     return (this.x2 > this.x1) && (this.y2 > this.y1) && (!isNaN(this.x1 + this.x2 + this.y1 + this.y2));
   }
-  isPointInside(point) {
-    let { x, y } = point;
+  isPointInside(x, y) {
     return (x > this.x1 && x < this.x2 && y > this.y1 && y < this.y2);
   }
-  isPointOnEdge(point) {
-    let { x, y } = point;
+  isPointOnEdge(x, y) {
     if (x === this.x1 && y > this.y1 && y < this.y2) return true; //left edge
     if (x === this.x2 && y > this.y1 && y < this.y2) return true; //right edge
     if (y === this.y1 && x > this.x1 && x < this.x2) return true //top edge
     if (y === this.y2 && x > this.x1 && x < this.x2) return true //top edge
     return false;
   }
-  touches(points) {
-    for (let point of points) {
-      if (this.isPointInside(point) || this.isPointOnEdge(point)) {
-        return true;
-      }
+  isOverlapping(boundry){
+    //return true If BOTH of the boundry's x are OUTSIDE but BOTH of the y's ARE INSIDE. 
+    //OR IF y's are BOTH outside and x's are BOTH inside
+    return false;
+  }
+  static touches(boundry1, boundry2) {
+    let x = []; let y = [];
+
+    x[0] = boundry1.x1; y[0] = boundry1.y1;//top left
+    x[1] = boundry1.x2; y[1] = boundry1.y1;//top right
+    x[2] = boundry1.x1; y[2] = boundry1.y2//bottom left
+    x[3] = boundry1.x2; y[3] = boundry1.y2;//botttom right
+    for (let i = 0; i < 4; i++) {
+      if (boundry2.isPointInside(x[i], y[i]) || boundry2.isPointOnEdge(x[i], y[i])) return true;
+    }
+    x[0] = boundry2.x1; y[0] = boundry2.y1;//top left
+    x[1] = boundry2.x2; y[1] = boundry2.y1;//top right
+    x[2] = boundry2.x1; y[2] = boundry2.y2//bottom left
+    x[3] = boundry2.x2; y[3] = boundry2.y2;//botttom right
+    for (let i = 0; i < 4; i++) {
+      if (boundry1.isPointInside(x[i], y[i]) || boundry1.isPointOnEdge(x[i], y[i])) return true;
     }
     return false;
   }
