cf18de8665d301f61c1e3d38ed8b61a76c5180b2
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-01-02 21:03:59 -0400

    They are now CONSTRUCTED in a 'type' way, and you can use the createInstance(...) method to make a 'specific' part from a 'type' of one, and add the specifics like which actor owns it, where it is, where its facing, and what looks like.

--- classes/part.js
+++ classes/part.js
@@ -1,34 +1,27 @@
 import Point from './point.js';
 import Transpose from './transpose.js';
 export default class Part {
+  partTypeName = undefined;
   name = undefined;
   position = undefined;
   facing = 0;
   owner = undefined;
   polygon = null;
   appearance = undefined;
-  particleGenerator = undefined; // Add this property
+  particleGenerator = undefined; //must be attached.
   
-  constructor(name, x, y, polygon, facing, parent) {
-    this.name = name;
-    this.position = new Point(x, y);
-    this.polygon = polygon;
-    this.facing = (facing) ? facing : 0;
-    this.parent = parent;
+  constructor(partTypeName, polygon) {
+    this.partTypeName = partTypeName;    
+    this.polygon = polygon;  
   }
-  
-  attachParticleGenerator(generator) {
-    this.particleGenerator = generator;
-    generator.attachedPart = this;
-    return this;
-  }
-  
-  updateParticleGenerator() {
-    if (this.particleGenerator) {      
-      let worldCoords = Transpose.childToWorld (this,this.parent);
-      this.particleGenerator.setPosition(worldCoords);
-      this.particleGenerator.setFacing(this.parent.facing + this.facing);
-    }
+  createInstance (owner, name, offsetFromActorOrigin, facing, appearance){
+    let attachedPart = new Part(this.partTypeName, this.polygon);
+    attachedPart.owner = owner;
+    attachedPart.name = name;
+    attachedPart.position = offsetFromActorOrigin;
+    attachedPart.facing = facing;
+    attachedPart.appearance = appearance;
+    return attachedPart;
   }
   isPart() {
     return true;
