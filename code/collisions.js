8eaac47752cc23df5271d06849d706578d7d2563
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-01-19 17:51:36 -0400

    should be same as they were in the original branch.

---
+++ code/collisions.js
@@ -0,0 +1,88 @@
+import Director from "./director.js";
+import Point from "./point.js";
+import Boundry from './boundry.js';
+class Collision {
+  constructor(actor, otherActor, overlap) {
+    this.actor = actor;
+    this.otherActor = otherActor;
+    this.overlap = overlap;
+  }
+}
+export default class Collisions {
+  static initialize() {
+    this.collisions = new Map();
+  }
+  static callActorCollisionEvents(collision) {
+    if (collision.actor.collisionFn) {
+      collision.actor.collisionFn(collision.otherActor);
+    }
+    if (collision.otherActor.collisionFn) {
+      collision.otherActor.collisionFn(collision.actor);
+    }
+  }
+  static getCollisions(quadTree) {
+    let collisions = new Map();
+    for (let actor of Director.actors.values()) {
+      if (actor.collides) {
+         let actorBoundry =  new Boundry(
+          actor.position.x - actor.radius, actor.position.y - actor.radius, //upper left
+          actor.position.x + actor.radius, actor.position.y + actor.radius//lower right
+        );
+        let potentialCollisions = quadTree.findInRange(actorBoundry);
+        for (let otherActor of potentialCollisions) {
+          if (actor !== otherActor) {
+            let overlap = (actor.radius + otherActor.radius) - Point.distance(actor.position, otherActor.position);
+            if (overlap >= 0) {
+              let collisionID = this.#makeCollisionID(actor, otherActor);
+              let altCollisionID = this.#makeAltCollisionID(actor, otherActor);
+              if (!collisions.has(collisionID) && !collisions.has(altCollisionID)) {
+                collisions.set(collisionID, new Collision(actor, otherActor, overlap));
+              }
+            }
+          }
+        }
+      }
+    }
+    return collisions;
+  }
+  static handleCollisionPhysics(collision) {
+    let m1 = collision.actor.mass();
+    let m2 = collision.otherActor.mass();
+    let totalMass = m1 + m2;
+    if (totalMass <= 0) return;
+    //calculate the normal vector
+    let p1 = Point.from(collision.actor.position);
+    let p2 = Point.from(collision.otherActor.position);
+    let normalAxis = Point.from(p1);
+    Point.sub(normalAxis, p2);
+    Point.normalize(normalAxis);
+    //move the objects back along the normal axis by 1/2 of the offset.
+    let partialOverlap = collision.overlap / 2;
+    let moveA = Point.from(normalAxis);
+    Point.scale(moveA, partialOverlap);
+    Point.add(collision.actor.position, moveA);
+    let moveB = Point.from(normalAxis);
+    Point.scale(moveB, partialOverlap);
+    Point.sub(collision.otherActor.position, moveB);
+    //Transfer momentum and change velocities.
+    let tangentAxis = new Point(-normalAxis.y, normalAxis.x);
+    let v1n = Point.dot(collision.actor.velocity, normalAxis);        //Get scalar velocity along each axis..
+    let v1t = Point.dot(collision.actor.velocity, tangentAxis);
+    let v2n = Point.dot(collision.otherActor.velocity, normalAxis);   //For both objects..
+    let v2t = Point.dot(collision.otherActor.velocity, tangentAxis);
+    let v1nF = (v1n * (m1 - m2) + 2 * m2 * v2n) / (totalMass);           //1-d tranfer of momentum but only on normal axis..
+    let v2nF = (v2n * (m2 - m1) + 2 * m1 * v1n) / (totalMass);
+    let v1nFV = Point.scale(Point.from(normalAxis), v1nF * collision.actor.bounceCoefficient);
+    let v2nFV = Point.scale(Point.from(normalAxis), v2nF * collision.otherActor.bounceCoefficient);
+    let v1tFV = Point.scale(Point.from(tangentAxis), v1t * collision.actor.bounceCoefficient);
+    let v2tFV = Point.scale(Point.from(tangentAxis), v2t * collision.otherActor.bounceCoefficient);
+    collision.actor.velocity = Point.add(v1nFV, v1tFV);              //final velocity is normal and tangent added back together.
+    collision.otherActor.velocity = Point.add(v2nFV, v2tFV);
+  }
+  static #makeAltCollisionID(actor, otherActor) {
+    return `${otherActor.name}|${actor.name}`;
+  }
+  static #makeCollisionID(actor, otherActor) {
+    return `${actor.name}|${otherActor.name}`;
+  }
+}
---
+++ code/view.js
@@ -0,0 +1,171 @@
+import Point from './point.js';
+
+export default class View {
+  backgroundPressed = false;
+  backgroundPressedCoordinate = null;
+  backgroundColor = "#000";
+  bounds = undefined;
+  camera = Point.zero();
+  minimumZoom = 0.1;
+  mouse = new Point(0, 0);
+  screenCenter = null; // Initialize as null
+  zoomFactor = 10;
+
+  constructor(background, canvas, canvasContainer) {
+    if (!canvas) {
+      //assumes a blank page..
+      this.canvas = document.createElement('canvas');
+      this.canvas.style.margin = 0;
+      this.canvas.style.padding = 0;
+      this.canvas.id = 'canvas';
+ 
+      this.canvasContainer = window;
+      this.externalGUI = false;
+    } else {
+      //when working within an HTML environment with its own controls..
+      this.canvas = canvas;
+      this.canvasContainer = canvasContainer;
+      this.externalGUI = true;
+    }
+    this.context = this.canvas.getContext('2d');
+    this.mouse.buttonDown = false;
+    this.camera.zoom = 1; // Default zoom level
+    this.context = this.canvas.getContext('2d');
+    if (background) this.backgroundColor = background;
+    
+    // Only append canvas to body if we created it ourselves
+    if (!this.externalGUI) {
+      let body = document.getElementsByTagName('body')[0];
+      body.appendChild(this.canvas);
+      body.style.margin = 0;
+      body.style.padding = 0;
+    }
+    //this.canvas.onwheel = this.handleWheel;
+    this.canvas.addEventListener('wheel', this.handleWheel, { passive: false });
+    this.canvas.onmousemove = this.handleMouseMove;
+    this.canvas.onmousedown = this.handleMouseDown;
+    this.canvas.onmouseup = this.handleMouseUp;
+    this.canvas.oncontextmenu = this.handleContextMenu;
+    window.addEventListener('resize', this.resizeCanvas.bind(this));
+    this.resizeCanvas();  // This will set screenCenter and the bounds.
+  }
+  canSee(point, radius) {
+    let { x0, y0, x1, y1 } = this.bounds;
+    if (radius && typeof radius === 'number') {
+      x0 -= radius;
+      y0 -= radius;
+      x1 += radius;
+      y1 += radius;
+    }
+    return (
+      point.x >= x0 &&
+      point.y >= y0 &&
+      point.x <= x1 &&
+      point.y <= y1
+    );
+  }
+  #calcBounds() {
+    this.bounds = {
+      "x0": this.camera.x - this.screenCenter.x / this.camera.zoom,
+      "y0": this.camera.y - this.screenCenter.y / this.camera.zoom,
+      "x1": this.camera.x + this.screenCenter.x / this.camera.zoom,
+      "y1": this.camera.y + this.screenCenter.y / this.camera.zoom
+    }
+  }
+  clear() {
+    this.context.fillStyle = this.backgroundColor;
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  handleCameraDrag(actorMouseInteraction) {
+    if (!actorMouseInteraction) {
+      if (this.backgroundPressed && this.mouse.buttonDown) {
+        //drag occuring..         
+        let drag = Point.from(this.backgroundPressedCoordinate);
+        let inverseMouse = new Point(this.mouse.x * -1, this.mouse.y * -1)
+        Point.add(drag, inverseMouse);
+        Point.scale(drag, 1 / this.camera.zoom);
+        Point.add(this.camera, drag);
+        this.#calcBounds();
+        this.backgroundPressedCoordinate = Point.from(this.mouse);
+      }
+      if (this.backgroundPressed && !this.mouse.buttonDown) {
+        //drag ended.
+        this.backgroundPressed = false;
+        this.backgroundPressedCoordinate = null;
+      }
+      else if (!this.backgroundPressed && this.mouse.buttonDown) {
+        //initiated drag on background..        
+        this.backgroundPressed = true;
+        this.backgroundPressedCoordinate = Point.from(this.mouse);
+      }
+    }
+    else {
+      if (this.backgroundPressed) {
+        this.backgroundPressed = false;
+        this.backgroundPressedCoordinate = null;
+      }
+    }
+  }
+  //Keep this as an arrow function or you will lose reference to "this".
+  handleContextMenu = (event) => {
+    event.preventDefault();
+    return false;
+  }
+
+  //Keep this as an arrow function or you will lose reference to "this".
+  handleMouseMove = (event) => {
+    this.mouse.x = event.clientX;
+    this.mouse.y = event.clientY;
+  };
+  //Keep this as an arrow function or you will lose reference to "this".
+  handleMouseDown = (event) => {
+    if (event.button === 0) {
+      this.mouse.buttonDown = true;
+    } else if (event.button === 2) {
+      event.preventDefault();
+      return false;
+    }
+  };
+  //Keep this as an arrow function or you will lose reference to "this".
+  handleMouseUp = (event) => {
+    this.mouse.buttonDown = false;
+  };
+  //Keep this as an arrow function or you will lose reference to "this".
+  handleWheel = (event) => {
+    let zoomChange = this.camera.zoom * -Math.sign(event.deltaY) / this.zoomFactor;
+    let oldZoom = this.camera.zoom;
+    this.camera.zoom = this.camera.zoom + zoomChange;
+    let xdiff = this.canvas.width / oldZoom - this.canvas.width / this.camera.zoom;
+    let ydiff = this.canvas.height / oldZoom - this.canvas.height / this.camera.zoom;
+    let xratio = (this.mouse.x - (this.canvas.width / 2)) / this.canvas.width;
+    let yratio = (this.mouse.y - (this.canvas.height / 2)) / this.canvas.height;
+    let xchange = xdiff * xratio;
+    let ychange = ydiff * yratio;
+    this.camera.x += xchange;
+    this.camera.y += ychange;
+    this.camera.zoom = Math.max(this.minimumZoom, this.camera.zoom);
+    this.#calcBounds();
+  }
+  resizeCanvas() {
+    
+    if (this.canvasContainer === window) {
+      this.canvas.width = window.innerWidth;
+      this.canvas.height = window.innerHeight;
+      console.log ('resizing with window');
+    } else {
+      console.log ('before resizing container:'+this.canvasContainer.clientWidth+','+this.canvasContainer.clientHeight);
+      this.canvas.width = this.canvasContainer.clientWidth;
+      this.canvas.height = this.canvasContainer.clientHeight;
+      console.log ('after reszing  container'+this.canvasContainer.clientWidth+','+this.canvasContainer.clientHeight);
+    }
+    console.log (`w,h: ${this.canvas.width}, ${this.canvas.height}`)
+    if (!this.screenCenter) {
+      this.screenCenter = new Point(this.canvas.width / 2, this.canvas.height / 2);
+    } else {
+      this.screenCenter.x = this.canvas.width / 2;
+      this.screenCenter.y = this.canvas.height / 2;
+    }
+
+    this.#calcBounds();
+  }
+}
