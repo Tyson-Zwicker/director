47cc923c52d2113cf004ba509e934df9fba59c4d
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-01-24 19:48:13 -0400

    Text metrics worked out.  Textbox text is centered in both axis.

--- code/draw.js
+++ code/draw.js
@@ -3,7 +3,7 @@ export default class Draw {
   constructor(context2d) {
     this.g = context2d;
   }
-  box2(x1, y1, x2, y2, hexColor) {
+  boxHex(x1, y1, x2, y2, hexColor) {
     if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) throw new Error(`line.draw bad: coordinates (${x1},${y1}) (${x2},${y2})`);
 
     this.g.strokeStyle = hexColor;
@@ -11,35 +11,49 @@ export default class Draw {
     this.g.rect(x1, y1, x2 - x1, y2 - y1);
     this.g.stroke();
   }
-  fillbox2(x1, y1, x2, y2, hexColor) {
+  fillboxHex(x1, y1, x2, y2, hexColor) {
     if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) throw new Error(`line.draw bad: coordinates (${x1},${y1}) (${x2},${y2})`);
 
     this.g.fillStyle = hexColor;
     this.g.beginPath();
-    this.g.rect(x1, y1, x2 - x1, y2 - y1);
+    this.g.fillRect(x1, y1, x2 - x1, y2 - y1);
     this.g.fill();
   }
-  line2(line, hexColor) {
+  lineHex(line, hexColor) {
     this.g.strokeStyle = hexColor;
     this.g.beginPath();
     this.g.moveTo(line.p0.x, line.p0.y);
     this.g.lineTo(line.p1.x, line.p1.y);
     this.g.stroke();
   }
-  circle2(x, y, radius, hexColor) {
+  circleHex(x, y, radius, hexColor) {
     this.g.fillStyle = hexColor;
     this.g.beginPath();
     this.g.arc(x, y, radius, 0, Math.PI * 2);
     this.g.fill();
   }
-  box(x1, y1, x2, y2, colorOrGradient) {
+  box(x1, y1, x2, y2, colorOrGradient, lineWidth) {
+    if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) throw new Error(`line.draw bad: coordinates (${x1},${y1}) (${x2},${y2})`);
+    if (colorOrGradient instanceof Color) {
+      this.g.strokeStyle = colorOrGradient.asHex();
+    } else {
+      this.g.strokeStyle = colorOrGradient;
+    }
+    this.g.lineWidth = lineWidth;
+    this.g.beginPath();
+    this.g.rect(x1, y1, x2 - x1, y2 - y1);
+    this.g.stroke();
+  }
+  fillBox(x1, y1, x2, y2, colorOrGradient) {
     if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) throw new Error(`line.draw bad: coordinates (${x1},${y1}) (${x2},${y2})`);
     if (colorOrGradient instanceof Color) {
       this.g.fillStyle = colorOrGradient.asHex();
     } else {
       this.g.fillStyle = colorOrGradient;
     }
+    this.g.beginPath();
     this.g.fillRect(x1, y1, x2 - x1, y2 - y1);
+    this.g.fill();
   }
   line(x1, y1, x2, y2, w, colorOrGradient) {
     if (isNaN(w)) throw new Error('no width defined.');
@@ -56,21 +70,28 @@ export default class Draw {
     this.g.stroke();
   }
   //x,y are the upper left hand corner
-  getTextSize(text, fontSize) {
-    this.g.textBaseline = 'top';
-    this.g.textAlign = 'left';
-    this.font = `${this.fontSize}px monospace`;
-    let metrics = this.g.measureText(text);
-    return metrics.width;
-  }
-  textBox(x1, y2, x2, y2, text, fontSize, fontName, appearance) {
-    Director.view.context.fillText(this.text, screenPosition.x, screenPosition.y);
-    this.fillbox2(x1, y1, x2, y2, appearance.fillHex);
-    this.box2(x1, y1, x2, y2, appearance.strokeHex);
-    this.fillStyle = appearance.text;
+  getTextSize(text, fontSize, fontName) {
     this.g.textBaseline = 'top';
     this.g.textAlign = 'left';
     this.font = `${this.fontSize}px ${fontName}}`;
-    this.g.drawText (x,y, text);
+    let metrics = this.g.measureText(text);
+    return { "width": metrics.width, "height": (metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent) ?? fontSize };
+  }
+  textBox(x1, y1, x2, y2, text, fontSize, fontName, appearance) {
+    this.fillBox(x1, y1, x2, y2, appearance.fillHex);
+    this.box(x1, y1, x2, y2, appearance.stokeHex, appearance.lineWidth);
+    
+    this.g.fillStyle = appearance.textHex;
+    this.g.textBaseline = 'top';
+    this.g.textAlign = 'left';
+    this.g.font = `${fontSize}px ${fontName}`;
+    let textSize = this.getTextSize(text, fontSize, fontName);
+    let tx = x1 + ((x2 - x1) - textSize.width) / 2;
+    let ty = y1 + ((y2 - y1) - textSize.height) / 2;
+    console.log (`box is ${y1-y1} tall.`);
+    console.log (`text is ${textSize.height} tall.`);
+    
+    
+    this.g.fillText(text, tx, ty);
   }
 }
--- tests/draw/test.js
+++ tests/draw/test.js
@@ -1,5 +1,6 @@
 import Draw from '../../code/draw.js';
 import Color from '../../code/color.js';
+import Appearance from '../../code/appearance.js';
 // ----------> PRIME MOVER <-------------
 init();
 export default function init() {
@@ -9,51 +10,84 @@ export default function init() {
   let context = canvas.getContext('2d');
   context.fillStyle = '#223';
   context.fillRect(0, 0, canvas.width, canvas.height);
+
   let draw = new Draw(context);
   let red = new Color(15, 0, 0, 0.5);
   draw.line(50, 50, 100, 100, 10, red);
   let green = new Color(10, 15, 0, 0.5);
   draw.line(50, 100, 100, 50, 20, green);
 
-  let spacing = 20;
+  let spacing = 10;
   for (let x = 0; x < 16; x++) {
     for (let y = 0; y < 16; y++) {
-      let red = Math.ceil(15 - x);
-      let blue = Math.ceil(15 - y);
-      let green = Math.ceil(15 - (red + blue) / 2);
+      let ravg = 15 - x;
+      let bavg = x;
+      let gavg = y;
+      let red = Math.ceil(ravg);
+      let green = Math.ceil(gavg);
+      let blue = Math.ceil(bavg);
+
       let color = new Color(red, green, blue, 0.3);
-      draw.box(x * spacing, y * spacing, (x + 1) * spacing, (y + 1) * spacing, color);
+      draw.fillBox(
+        x * spacing,
+        y * spacing,
+        (x + 1) * spacing,
+        (y + 1) * spacing, color);
     }
   }
 
   for (let x = 0; x < 16; x++) {
     for (let y = 0; y < 16; y++) {
-      let red = Math.ceil(15 - x);
-      let blue = Math.ceil(15 - y);
-      let green = Math.ceil(15 - (red + blue) / 2);
-      let color = new Color(red, green,blue, 1);
-      draw.box(400 + x * spacing, y * spacing, 400 + (x + 1) * spacing, (y + 1) * spacing, color);
+      let ravg = 15 - x;
+      let bavg = x;
+      let gavg = y;
+      let red = Math.ceil(ravg);
+      let green = Math.ceil(gavg);
+      let blue = Math.ceil(bavg);
+      let color = new Color(red, green, blue, 1);
+      draw.fillBox(
+        200 + x * spacing,
+        y * spacing,
+        200 + (x + 1) * spacing,
+        (y + 1) * spacing, color);
     }
   }
 
   for (let x = 0; x < 16; x++) {
     for (let y = 0; y < 16; y++) {
-      let red = Math.ceil(15 - x);
-      let blue = Math.ceil(15 - y);
-      let green = Math.ceil(15 - (red + blue) / 2);
-      let color = new Color(red, green, blue).changeBrightness(0.5);
-      draw.box(400 + x * spacing, 400 + y * spacing, 400 + (x + 1) * spacing, 400 + (y + 1) * spacing, color);
+      let ravg = 15 - x;
+      let bavg = x;
+      let gavg = y;
+      let red = Math.ceil(ravg);
+      let green = Math.ceil(gavg);
+      let blue = Math.ceil(bavg);
+      let color = new Color(red, green, blue).changeBrightness(.3);
+      draw.fillBox(
+        200 + x * spacing,
+        200 + y * spacing,
+        200 + (x + 1) * spacing,
+        200 + (y + 1) * spacing, color);
     }
   }
 
-   for (let x = 0; x < 16; x++) {
+  for (let x = 0; x < 16; x++) {
     for (let y = 0; y < 16; y++) {
-      let red = Math.ceil(15 - x);
-      let blue = Math.ceil(15 - y);
-      let green = Math.ceil(15 - (red + blue) / 2);
-      let color = new Color(red, green, blue).changeBrightness(0.33);
-      draw.box( x * spacing, 400 + y * spacing,  (x + 1) * spacing, 400 + (y + 1) * spacing, color);
+      let ravg = 15 - x;
+      let bavg = x;
+      let gavg = y;
+      let red = Math.ceil(ravg);
+      let green = Math.ceil(gavg);
+      let blue = Math.ceil(bavg);
+      let color = new Color(red, green, blue).changeBrightness(0.8);
+      draw.fillBox(
+        x * spacing,
+        200 + y * spacing,
+        (x + 1) * spacing,
+        200 + (y + 1) * spacing, color);
     }
-  }      
+  }
+  let appearance = new Appearance ('test','#040','#0a0','#fff',2);
+  console.log (appearance);
+  draw.textBox (400,5,500,45, 'Text Box!', 16, 'monospace', appearance);
 }
 
