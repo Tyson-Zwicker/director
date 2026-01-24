17367a4c2cb96cfbc7c49e668db2e4469338a2b5
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-01-24 18:04:42 -0400

    working on a gui to place list/text/button aruond the edges of the canvas (and a floater for popups)

--- code/draw.js
+++ code/draw.js
@@ -5,7 +5,7 @@ export default class Draw {
   }
   box2(x1, y1, x2, y2, hexColor) {
     if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) throw new Error(`line.draw bad: coordinates (${x1},${y1}) (${x2},${y2})`);
-    
+
     this.g.strokeStyle = hexColor;
     this.g.beginPath();
     this.g.rect(x1, y1, x2 - x1, y2 - y1);
@@ -13,24 +13,24 @@ export default class Draw {
   }
   fillbox2(x1, y1, x2, y2, hexColor) {
     if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) throw new Error(`line.draw bad: coordinates (${x1},${y1}) (${x2},${y2})`);
-    
+
     this.g.fillStyle = hexColor;
     this.g.beginPath();
     this.g.rect(x1, y1, x2 - x1, y2 - y1);
     this.g.fill();
   }
-  line2 (line, hexColor){
+  line2(line, hexColor) {
     this.g.strokeStyle = hexColor;
     this.g.beginPath();
-    this.g.moveTo (line.p0.x, line.p0.y);
-    this.g.lineTo (line.p1.x, line.p1.y);
+    this.g.moveTo(line.p0.x, line.p0.y);
+    this.g.lineTo(line.p1.x, line.p1.y);
     this.g.stroke();
   }
-  circle2 (x,y,radius, hexColor){
+  circle2(x, y, radius, hexColor) {
     this.g.fillStyle = hexColor;
     this.g.beginPath();
-    this.g.arc (x,y,radius,0,Math.PI*2);
-    this.g.fill();    
+    this.g.arc(x, y, radius, 0, Math.PI * 2);
+    this.g.fill();
   }
   box(x1, y1, x2, y2, colorOrGradient) {
     if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) throw new Error(`line.draw bad: coordinates (${x1},${y1}) (${x2},${y2})`);
@@ -54,5 +54,23 @@ export default class Draw {
     this.g.moveTo(x1, y1);
     this.g.lineTo(x2, y2);
     this.g.stroke();
+  }
+  //x,y are the upper left hand corner
+  getTextSize(text, fontSize) {
+    this.g.textBaseline = 'top';
+    this.g.textAlign = 'left';
+    this.font = `${this.fontSize}px monospace`;
+    let metrics = this.g.measureText(text);
+    return metrics.width;
+  }
+  textBox(x1, y2, x2, y2, text, fontSize, fontName, appearance) {
+    Director.view.context.fillText(this.text, screenPosition.x, screenPosition.y);
+    this.fillbox2(x1, y1, x2, y2, appearance.fillHex);
+    this.box2(x1, y1, x2, y2, appearance.strokeHex);
+    this.fillStyle = appearance.text;
+    this.g.textBaseline = 'top';
+    this.g.textAlign = 'left';
+    this.font = `${this.fontSize}px ${fontName}}`;
+    this.g.drawText (x,y, text);
   }
 }
--- code/gui.js
+++ code/gui.js
@@ -1,44 +1,142 @@
 import Boundry from './boundry.js';
-import Appearance from './appearance';
-class GUIPane {
-  constructor(orientation, hidden) {
-    if (typeof oriention !== 'string' || (orientation !== 'vertical' && orientation !== 'horizontal'))
-      throw new Error(`GIUPane.constructor: orientation must be "vertical" or "horizontal" [${orientation}`);
-    if (typeof hidden !=='boolean') 
-      throw new Error(`GIUPane.constructor: hidden must be true or false [${orientation}`);
-    this.orientation = orientation;
-    this.hidden=hidden;
-    this.items = new map();
+import Appearance from './appearance.js';
+import Button from '.button.js';
+
+class GUI {
+  static paneNames = ['top', 'bottom', 'left', 'right', 'float'];
+  constructor(colWidth, rowHeight) {
+    if (typeof colWidth !== 'number' || typeof rowHeight !== 'number') throw new Error(`GUI.constructor colWidth and rowHeight should be numbers [${colWidth},${rowHeight}].`);
+    let w = Director.view.canvas.width;
+    let h = Director.view.canvas.height;
+    this.panes = new Map();
+    this.panes.set('top', { "boundry": this.#topBoundry(), "items": [], 'activeList': undefined });
+    this.panes.set('bottom', { "boundry": this.#bottomBoundry(), "items": [], 'activeList': undefined });
+    this.panes.set('left', { "boundry": this.#leftBoundry(), "items": [], 'activeList': undefined });
+    this.panes.set('right', { "boundry": this.#rightBoundry(), "items": [], 'activeList': undefined });
+    this.lists = new Map();
+  }
+  #topBoundry() {
+    return new Boundry(0, 0, w, rowHeight);
+  }
+  #bottomBoundry() {
+    return new Boundry(0, h - rowHeight, w, h);
+  }
+  #leftBoundry() {
+    return new Boundry(0, rowHeight, colWidth, h - rowHeight);
+  }
+  #rightBoundry() {
+    return new Boundry(w - colWidth, rowHeight, w, h - rowHeight);
+  }
+  //This gets called from View.resizeCanvas().  That's the only place its called from. 2026-01-24
+  resize() {
+    this.panes.get('top').boundry = this.#topBoundry();
+    this.panes.get('bottom').boundry = this.#bottomBoundry();
+    this.panes.get('left').boundry = this.#leftBoundry();
+    this.panes.get('right').boundry = this.#rightBoundry();
+  }
+  getButton(label, normalAppearance, hoveredAppearance, pressedAppearance, toggle, fn) {
+    if (typeof toggle !== 'boolean') throw new Error(`GUI.addButton: toggle must be boolean [${toggle}].`);
+    let b = new Button(hoveredAppearance, pressedAppearance, fn, toggle);
+    let newButton = {
+      "type": 'button',
+      "text": label,
+      "appearance": normalAppearance,
+      "button": b,
+      "bounds": undefined,
+      "visible": true,
+    }
+    b.guiControl = newButton;
+    return newButton;
+  }
+  addButton(pane, label, normalAppearance, hoveredAppearance, pressedAppearance, toggle, fn) {
+    if (!GUI.paneNames.includes(pane)) throw new Error(`GUI.addButton: Pane must be top,bottom,left, right or float [${pane}].`);
+    this.panes.get(pane).push(this.getButton(label, normalAppearance, hoveredAppearance, pressedAppearance, toggle, fn));
+  }
+  getText(label, appearance) {
+    this.panes.get(pane).push({
+      "type": 'text',
+      "text": label,
+      "appearance": appearance,
+      "button": undefined,
+      "bounds": undefined,
+      "visible": true
+    });
+  }
+  addText(pane, label, appearance) {
+    if (!GUI.paneNames.includes(pane)) throw new Error(`GUI.addText: Pane must be top,bottom,left, right or float [${pane}].`);
+    this.panes.get(pane).push(getText(label, appearance));
+  }
+  getList(label, normalAppearance, hoveredAppearance, pressedAppearance, listName, listItems) {
+    if (!Array.isArray(items)) throw new Error(`GUI.addList: Pane must be top,bottom,left, right or float [${pane}].`);
+    if (this.lists.has(listName)) throw new Error(`GUI.addList: Pane ${pane} already contains list with name ${listName}`);
+    let b = new Button(hoveredAppearance, pressedAppearance, (owner) => { Director.gui.showList(owner.listName); }, false);
+    let newList = {
+      "type": 'list',
+      "listName": listName,
+      "text": label,
+      "appearance": normalAppearance,
+      "button": b,
+      "bounds": undefined,
+      "visible": true
+    };
+    b.guiControl = this; //Binding the button to the new control.. so the function points at this.. so it can pass this list's name to showList()..
+    //make sure items is itemsList are actually buttons and bind those buttons to this list..
+    if (!Array.isArray(listItems)) throw new Error(`GUI.addList: Pane ${pane} already contains list with name ${listName}`);
+    for (item of items) {
+      if (typeof item.type === 'string' && item.type === 'button') {
+        item.listName = listName;
+      } else throw new Error(`GUI.addList: List item is not a button ${item}`);
+    }
+    newList.items = items;
+    return newList;
+  }
+  addList(pane, label, normalAppearance, hoveredAppearance, pressedAppearance, listName, listItems) {
+    if (!GUI.paneNames.includes(pane)) throw new Error(`GUI.addList: Pane must be top,bottom,left, right or float [${pane}].`);
+    let newList = getList(label, normalAppearance, hoveredAppearance, pressedAppearance, listName, listItems);
+    newList.paneName = pane;
+    this.lists.set(listName, newList);
+    this.panes.get(pane).push(newList);
+  }
+  showList(listName) {
+    // Pane def: { "boundry": this.#topBoundry(), "items": [], 'activeList': undefined }
+    //1. Hide the controls in this list's pane..
+    let list = this.lists.get(listName);
+    let pane = this.panes.get(list.paneName);
+    let paneItems = pane.items;
+    for (item of paneItems) {
+      item.visible = false;
+    }
+    //2.. Make its items visible;
+    let listItems = list.listItems;
+    for (let listItem of listItems) {
+      listItem.visible = true;
+    }
+    //3.. Set the active list for this pane..
+    pane.activeList = listName;
+    // Now next draw the list will be shown.  Its should probably be drawn with an offset so the list matches
+    //where the button used to be...
+  }
+  hideList(listname) {
+    //1. Show the controls in this list's pane..
+    let list = this.lists.get(listName);
+    let pane = this.panes.get(list.paneName);
+    let paneItems = pane.items;
+    for (item of paneItems) {
+      item.visible = true;
+    }
+    //2.. Make its items INvisible;
+    let listItems = list.listItems;
+    for (let listItem of listItems) {
+      listItem.visible = false;
+    }
+    //3.. Set the active list for this pane.. to nothing..
+    pane.activeList = undefined;
+  }
+  draw() {
+    //top and bottom go horizontally.
+    //calculate the width of all the parts put together..
+    //use that to set an offset from the left side = (totalwidth-neededwidth) /2
+    //the size something needs is basically just the width of the text + margin*2
+    
   }
 }
-class GUIControl{ //Appearance is currently governed by button..
-  //constructor (bounds, text, appearance
-}
-
-/*
-context.clip() to restrict canvas operation so this doesn't get over-written
-Philosophy:
-There are four panes surrounding the window (top, bottom, left, right)
-They may be shown or hidden.
-They have cells.  Top and Bottom have horizontal orientation, Left and right are vertical.
-They write from top to bottom and left to right.
-Think of each panel like a menu.
-  -It can have items you just pick (normal button)
-  -It can have settings you toggle.
-  -It can have a sub menu
-  -It can also just have some text (That's just an unclickable button really)
-
--buttons and toggles are easy (the button class already does all the logic)
--menus could be handled by temporarily replacing the panel with another panel, with the same controls. 
-  -make it recursive for sub-sub panels..
--
--radio buttons just take care of themselves because they are a sub menu of toggles...
-  -but the button you press should have a title and show the current selection, 
-  -or maybe it just has a text field above it that says like "mode:" and the button that shows
-  the submenu's current label is the toggled option..\
-  -in which case some level of recognition of "radio button" is in order, I guess.
-
-controls can be visible or hidden.
-The director has these panes when initialized, they need only be assigned controls.
- 
-*/
