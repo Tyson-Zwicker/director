1038eccdfeeb6c7023c564988ddafd89197731b6
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-01-27 17:57:14 -0400

    Panel and Element have their own class now.  Introduces 'float' as location (for lists and context menus/popups).

--- code/guielement.js
+++ code/guielement.js
@@ -1,9 +1,29 @@
-import Boundry from './boundry.js';
-import Point from './point.js';
-import Check from './check.js';
-import GUI from './gui.js';
-import Director from './director.js';
+
 
 class GUIElement {
-
+  constructor (direction, text, appearance, shadowAppearance, hoverAppearance, pressedAppearance, button, value){
+    let bounds =  measureItem(direction, text);
+    this.bounds = bounds;
+    this.active = true;//TODO: this replaces what "visible" used to do.
+    this.text = text;
+    this.drawnBounds = undefined;
+    this.appearance = appearance;
+    this.shadowAppearance = shadowAppearance;
+    //This is enough for the textbox, other Element Types (button and list)
+    //require additional properties assigned in the get control methods..    
+    this.hoveredAppearance = hoveredAppearance;
+    this.pressedAppearance = pressedAppearance;    
+    this.button = undefined; //assign at get when button is created depending on type..
+    this.selectedValue = undefined //Only used by lists..
+  }
+  measureItem() {
+    //This is the items personal bounds.  They are not screen coordinates,
+    //they describe the shape of this one item, regardless of its position in the panel.
+    let textSize = this.renderer.getTextSize(text, this.fontSize, this.fontName);
+    if (direction ==='up' || direction ==='down'){
+      return new Boundry (0,0,GUI.columnWidth, textSize.height);
+    }else{
+      return new Boundry (0,0,textSize.width, GUI.rowHeight);
+    }
+  }
 }
--- code/guipanel.js
+++ code/guipanel.js
@@ -1,3 +1,9 @@
+import Boundry from './boundry.js';
+import Point from './point.js';
+import Check from './check.js';
+import GUI from './gui.js';
+import Director from './director.js';
+
 class GUIPanel {
   elements = [];
   listElements = new Map();
@@ -9,6 +15,7 @@ class GUIPanel {
     if (location !== 'float') this.recalculate(); //Sets direction, boundry and offset.
     else if (location !== 'float') this.calculateFloat(parentElement);
   }
+
   draw() {
     let cursor = Point.from(this.offset);
     for (let element of this.elements) {
@@ -22,56 +29,64 @@ class GUIPanel {
       cursor.y += direction.y * (gap + element.bounds.height);
     }
   }
+  
   showList(listElement) {
     let floatingPanel = new Panel('float', listElement);
     this.listPanel = floatingPanel;
     this.activeList = listElement;
   }
+  
   hideList() {
     this.activeList = undefined;
     this.floatingPanel = undefined;
   }
+  
   calculateFloat(items, direction, listElement) {
     let itemsWidth = this.#getFloatElementsCollectiveWidth(direction);
     let itemsHeight = this.#getFloatElementsCollectiveHeight(direction);
+    let elementBounds = listElement.bounds;
     if (direction === 'up') {
       direction = new Point(0, 1);
       this.boundry = new Boundry(
-        listItem.bounds.x1, listItem.bounds.y1-itemsHeight,
-        listItem.bounds.x1+itemsWidth, listItem.bounds.y1
+        elementBounds.x1, elementBounds.y1 - itemsHeight,
+        elementBounds.x1 + itemsWidth, elementBounds.y1
       );
-      this.offset = new Point(listItem.bounds.x1, listItem.bounds.y1);
-      
+      this.offset = new Point(elementBounds.x1, elementBounds.y1);
+
     }
     if (direction === 'down') {
       direction = new Point(0, -1);
       this.boundry = new Boundry(
-        listItem.x1, listItem.y2,
-        listItem.x1 + itemsWidth, listItem.y2 + itemsHeight
+        elementBounds.x1, elementBounds.y2,
+        elementBounds.x1 + itemsWidth, elementBounds.y2 + itemsHeight
       );
-      this.offset = new Point(listItem.bounds.x1, listItem.bounds.y1);
+      this.offset = new Point(elementBounds.x1, elementBounds.y2);
     }
     if (direction === 'left') {
       direction = new Point(1, 0);
       this.boundry = new Boundry(
+        elementBounds.x1 - itemsWidth, elementBounds.y1,
+        elementBounds.x1, elementBounds.y1 + GUI.columnHeight,
+
       );
-      this.offset = new Point(listItem.bounds.x1, listItem.bounds.y1);
+      this.offset = new Point(elementBounds.x1, elementBounds.y1);
     }
     if (direction === 'right') {
       direction = new Point(-1, 0);
       this.boundry = new Boundry(
-
+        elementBounds.x2, elementBounds.y1,
+        elementBounds.x2 + itemsWidth, elementBounds.y1 + GUI.columnHeight,
       );
-      this.offset = new Point(listItem.bounds.x1, listItem.bounds.y1);
+      this.offset = new Point(elementBounds.x2, elementBounds.y1);
     }
-
   }
+  
   recalculate() {
     this.activeList = undefined; //If a list was opened, close it when they start fiddling with the window..
     let width = Director.view.canvas.width;
     let height = Director.view.canvas.height;
     let itemsWidth = this.#getElementsCollectiveWidth();
-    let itemsHeight = this.#getElementsCollectiveHeight();
+    let itemsHeight = this.#getElementsCollectiveHeight();    
     switch (this.location) {
       case 'top':
         this.direction = new Point(1, 0);
@@ -105,12 +120,17 @@ class GUIPanel {
         this.offset = new Point(this.boundry.x1, this.boundry.y1);
     }//end switch
   }
-  #getFloatElementsCollectiveWidth(direction){
-
+  
+  #getFloatElementsCollectiveWidth(direction) {
+    if (direction ==='up' || direction ==='down'){
+//TODO: finish.
+    }
   }
-  #getFloatElementsCollectiveHeight(direction){
-
+  
+  #getFloatElementsCollectiveHeight(direction) {
+//TODO: finish..
   }
+  
   #getElementsCollectiveWidth() {
     if (this.location === 'top' || this.location === 'bottom') {
       let w = 0;
@@ -120,6 +140,7 @@ class GUIPanel {
       return GUI.columnWidth;
     }
   }
+  
   #getElementsCollectiveHeight() {
     if (this.location === 'top' || this.location === 'bottom') {
       return GUI.columnWidth;
