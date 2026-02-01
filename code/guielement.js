76a4d8225ea69b8673c2742312b6640c37db8edd
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-01-31 23:50:53 -0400

    simplified constructor

--- code/guielement.js
+++ code/guielement.js
@@ -7,8 +7,8 @@ import Draw from './draw.js';
 import Check from './check.js';
 
 export default class GUIElement {
-  constructor (direction, text, appearance, shadowAppearance){    
-    this.bounds =  this.measureItem(direction, text);    
+  constructor ( text, appearance, shadowAppearance){    
+    this.bounds =  new Boundry (0,0,GUI.columnWidth, GUI.rowHeight); 
     this.active = true;
     this.text = text;
     this.drawnBounds = undefined;
@@ -16,9 +16,5 @@ export default class GUIElement {
     this.shadowAppearance = shadowAppearance;
     //This is enough for the textbox, other Element Types (button and list)
     //require additional properties assigned in the get control methods..        
-  }
-  measureItem(direction,text) {
-    return new Boundry (0,0,GUI.columnWidth, GUI.rowHeight);
-    
   }
 }
