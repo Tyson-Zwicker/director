000073695ea48976e54318f95cbc0b6214e06e89
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-01-31 23:22:12 -0400

    changed how bounds are calculated.

--- code/guielement.js
+++ code/guielement.js
@@ -7,12 +7,8 @@ import Draw from './draw.js';
 import Check from './check.js';
 
 export default class GUIElement {
-  constructor (direction, text, appearance, shadowAppearance, forceWidth, forceHeight){    
-    if (Check.num (forceWidth) && Check.num (forceHeight)){
-      this.bounds = new Boundry(0,0,forceWidth, forceHeight);
-    }else{
-      this.bounds =  this.measureItem(direction, text);
-    }
+  constructor (direction, text, appearance, shadowAppearance){    
+    this.bounds =  this.measureItem(direction, text);    
     this.active = true;
     this.text = text;
     this.drawnBounds = undefined;
@@ -23,14 +19,6 @@ export default class GUIElement {
   }
   measureItem(direction,text) {
     return new Boundry (0,0,GUI.columnWidth, GUI.rowHeight);
-    //This is the items personal bounds.  They are not screen coordinates,
-    //they describe the shape of this one item, regardless of its position in the panel.
-    let drawer = new Draw (Director.view.context);
-    let textSize = drawer.getTextSize(text, GUI.fontSize, GUI.fontName);
-    if (direction ==='up' || direction ==='down'){
-      return new Boundry (0,0,GUI.columnWidth, textSize.height+GUI.padding);
-    }else{
-      return new Boundry (0,0,textSize.width+GUI.padding, GUI.rowHeight);
-    }
+    
   }
 }
